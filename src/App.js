3b253daff48a031d8ac5baa8b79fb595101c903f
Matheus Carvalho <matheusgmc@hotmail.com>
2019-08-29 21:41:14 -0300

    Add Login Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-import UserForm from './components/user_form';
+
 import HttpService from './services/htttp_service';
+
+import RegisterUserForm from './components/register_user_form';
+import LoginUserForm from './components/login_user_form'
+
 import './App.css';
 
 
@@ -8,13 +12,16 @@ const httpUser = new HttpService()
 
 class App extends Component {
   render() {
-    return (<div className="container App">
+    return (
+    <div className="container App">
 
       <header className="App-header">
         <h1 className="App-title">Login Portal</h1>
       </header>
 
-      <UserForm registerUser={httpUser.registerUser}/>
+      <RegisterUserForm registerUser={httpUser.registerUser}/>
+      <LoginUserForm registerUser={httpUser.registerUser}/>
+
     </div>);
   }
 }
